1a9a18ca57e4c1893da62a5f46be56757660457d
Christopher <dagswebb@gmail.com>
2021-12-02 22:12:58 +0000

    Implemented web_helpers for feature testing

--- spec/features/enter_name_spec.rb
+++ spec/features/enter_name_spec.rb
@@ -1,9 +1,6 @@
 feature 'Enter names' do
     scenario 'submitting names' do
-        visit ('/')
-        fill_in :player_1_name, with: 'Christopher'
-        fill_in :player_2_name, with: 'Pikachu'
-        click_button 'Submit'
+        sign_in_and_play
         save_and_open_page
         expect(page).to have_content 'Christopher vs. Pikachu'
     end
--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,9 +1,6 @@
 feature 'View hit points' do
     scenario 'see Player 2 hit points' do
-        visit ('/')
-        fill_in :player_1_name, with: 'Christopher'
-        fill_in :player_2_name, with: 'Pikachu'
-        click_button 'Submit'
+        sign_in_and_play
         expect(page).to have_content 'Pikachu: 60HP'
     end
 end
